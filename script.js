8748f386ad738a664785473f1f98f050ec679444
jbabalfi-arch <jbabalfi@gmail.com>
2026-05-10 00:14:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-﻿function showSection(sectionId) {
-    // Ocultar todas las secciones
-    document.querySelectorAll('.content-section').forEach(section => {
-        section.classList.remove('active');
-    });
+﻿function showTab(tabId) {
+    document.querySelectorAll('.tab-pane').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.nav-item').forEach(btn => btn.classList.remove('active'));
     
-    // Quitar clase active de los botones
-    document.querySelectorAll('.nav-btn').forEach(btn => {
-        btn.classList.remove('active');
-    });
-    
-    // Mostrar la sección seleccionada
-    document.getElementById(sectionId).classList.add('active');
-    
-    // Activar el botón correspondiente
+    document.getElementById(tabId).classList.add('active');
     event.currentTarget.classList.add('active');
 }
